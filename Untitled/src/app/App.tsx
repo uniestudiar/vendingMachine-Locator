242a2fb3975fd14028705f2138e8b8aec12d9416
uniestudiar <bakereay@uni.edu>
2026-06-01 12:05:27 -0500

    Update App.tsx

--- Untitled/src/app/App.tsx
+++ Untitled/src/app/App.tsx
@@ -8,12 +8,14 @@ import ResetPassword from './components/ResetPassword';
 import PricingPage from './components/PricingPage';
 import AdminDashboard from './components/AdminDashboard';
 import OnboardingFlow from './components/OnboardingFlow';
+import HowItWorks from './components/HowItWorks';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/how-it-works" element={<HowItWorks />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/verify-email" element={<EmailVerification />} />
